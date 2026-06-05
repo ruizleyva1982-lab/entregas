c40828290116d51fd40939ebb893bf4a73fec98d
ruizleyva1982-lab <ruizleyva1982@gmail.com>
2026-06-05 00:10:44 -0500

    Update app_entregas.py

--- app_entregas.py
+++ app_entregas.py
@@ -19,7 +19,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive.readonly",
 ]
 
-SPREADSHEET_ID = "TU_SPREADSHEET_ID_AQUI"
+SPREADSHEET_ID = "1OQm27gEcI3-YylG03BpzbZewRqlYmkZydIhRklY7x1c"
 SHEET_NAME = "Hoja1"
 
 LIMA_TZ = pytz.timezone("America/Lima")
